67799e441241b8c6e245e3d1628492d8a6f63eac
ABiteofPi <payabiteofpi@gmail.com>
2026-02-04 15:59:42 +0100

    fix: added scss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/scss/main.scss'
 import router from './router'
 
 const app = createApp(App)
